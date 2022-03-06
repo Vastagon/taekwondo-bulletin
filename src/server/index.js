0ba0c176f2bf71984841607ac7a942e3f265d6ce
Vastagon <jacob.yancey29@yahoo.com>
2022-03-06 04:34:25 -0600

    fixed env file

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const dotenv = require('dotenv')
-
-dotenv.config();
-
+require('dotenv').config({ path: 'ENV_FILENAME' });
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,6 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// const uri = "mongodb+srv://Vastagon:Vastagon1@taekwondobulletincluste.7nvk4.mongodb.net/TaekwondoBulletinCluster?retryWrites=true&w=majority"
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser: true})
 const connection = mongoose.connection
