6c16809c2ea124448993bb2253b3059286d87e67
Vastagon <jacob.yancey29@yahoo.com>
2022-03-06 21:14:54 -0600

    Had a nights rest, hope this works

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 const uri = "mongodb+srv://Vastagon:Vastagon1@taekwondobulletincluste.7nvk4.mongodb.net/TaekwondoBulletinCluster?retryWrites=true&w=majority"
@@ -29,8 +29,13 @@ app.use("/exercises", exercisesRouter)///when going to exercises, it loads exerc
 app.use('/users', usersRouter)
 app.use("/blogposts", blogpostsRouter)
 
+
+const publicPath = path.join(__dirname, "Code", "taekwondo-bulletin", "public")
+app.use(express.static(publicPath))
+// console.log(__dirname, "Code", "taekwondo-bulletin", "public")
+
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(port, () =>{
