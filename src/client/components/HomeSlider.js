a29ef98154e0341fd81dbdeeff891bebed397f59
Vastagon <jacob.yancey29@yahoo.com>
2022-03-15 23:50:47 -0500

    removed node_modules

--- src/client/components/HomeSlider.js
+++ src/client/components/HomeSlider.js
@@ -1,9 +1,17 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
+import {Image} from "cloudinary-react"
 import data from "./data"
+import axios from "axios"
 
 export default function HomeSlider(props){
     const [currentImage, setCurrentImage] = useState(3)
+    const [imageData, setImageData] = useState({})
     let currentId = currentImage
+
+    useEffect(() =>{
+        axios.get("http://localhost:5000/eventsinfo")
+        .then(res => setImageData(res))
+    }, [])
 
     function clickRightArrowHome(){
         console.log("currentID " + currentId)
@@ -21,8 +29,6 @@ export default function HomeSlider(props){
     function clickLeftArrowHome(){
         console.log("currentID " + currentId)
 
-
-
         if(currentId <= 1){///changes ID and sets id back to 3 when at the beginning of the data
             setCurrentImage(data.length+1)
         }
@@ -38,7 +44,8 @@ export default function HomeSlider(props){
                 <div className="left-arrow arrow"></div>
                 
             </div>
-            <img alt="Cannot find Image" src={data[currentId-1].img} />
+            {/* <img alt="Cannot find Image" src={data[currentId-1].img} /> */}
+            <Image alt="Cannot find Image" className="event-image" cloud_name="dg9s57jo8" publicId={imageData.eventImg} />
             <div onClick={clickRightArrowHome} className="right-slider">
                 <div className="right-arrow arrow"></div>
             </div>
