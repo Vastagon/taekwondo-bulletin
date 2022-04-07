b6c953c5c4f7813fab361e08f5a1f987d78862d0
Vastagon <jacob.yancey29@yahoo.com>
2022-04-07 05:14:06 -0500

    started sliding animation when clicking arrow on homepage. Slide works but I need to add the second image

--- src/client/components/HomeSlider.js
+++ src/client/components/HomeSlider.js
@@ -3,8 +3,12 @@ import {Image} from "cloudinary-react"
 import axios from "axios"
 
 export default function HomeSlider(props){
+    ///currentImage is the variable holding the index of the array
     const [currentImage, setCurrentImage] = useState(0)
     const [imageData, setImageData] = useState({})
+    const [changeImageCheck, setChangeImageCheck] = useState()
+    const [clickedLeft, setClickedLeft] = useState()
+    const [clickedRight, setClickedRight] = useState()
     let currentId = currentImage
 
     ///Sets imageData to res.data
@@ -14,30 +18,118 @@ export default function HomeSlider(props){
     }, [])
 
     function clickRightArrowHome(){
-        if(currentId >= imageData.length-1){///changes ID and sets id back to 0 when at the end
-            setCurrentImage(-1)
-        }
+        setClickedRight(true)
+        setChangeImageCheck(true)
 
-        setCurrentImage(prev => prev + 1) //Adds 1 to ID
-        currentId = currentImage;
+
+        ///Removes slide left class after the slide is done
+        setTimeout(() =>{
+            ///Variable for conditional rendering to remove onClick until animation is finished
+            setChangeImageCheck(false)
+            setClickedRight(false)
+
+            if(currentId >= imageData.length-1){///changes ID and sets id back to 0 when at the end
+                setCurrentImage(-1)
+            }
+
+            setCurrentImage(prev => prev + 1) //Adds 1 to ID
+            currentId = currentImage;
+        }, 1000)
     }
+
+
     function clickLeftArrowHome(){
-        if(currentId <= 0){///changes ID and sets id back to the length of the array when at the beginning of the data
-            setCurrentImage(imageData.length)
-        }
-        setCurrentImage(prev => prev - 1) //Subtracts 1 to ID
-        currentId = currentImage;
+        setClickedLeft(true)
+        setChangeImageCheck(true)
+        ///Removes slide left class after the slide is done
+        setTimeout(() =>{
+            ///Variable for conditional rendering to remove onClick until animation is finished
+            setChangeImageCheck(false)
+            setClickedLeft(false)
+
+            if(currentId <= 0){///changes ID and sets id back to the length of the array when at the beginning of the data
+                setCurrentImage(imageData.length)
+            }
+            setCurrentImage(prev => prev - 1) //Subtracts 1 to ID
+            currentId = currentImage;
+
+        }, 1000)
+
     }
     return(
         <div id="home-slider" className="home-slider">
-            <div onClick={clickLeftArrowHome} className="left-slider">
+            {changeImageCheck ? ///Prevents clicking the arrow when animation is playing
+            <div id="left-slider" className="left-slider">
                 <div className="left-arrow arrow"></div>    
             </div>
-            <Image id="slider-image" alt="Cannot find Image" className="event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage]?.eventImg} />
-            <div onClick={clickRightArrowHome} className="right-slider">
-                <div className="right-arrow arrow"></div>
+            :
+            <div onClick={clickLeftArrowHome} id="left-slider" className="left-slider">
+                <div className="left-arrow arrow"></div>    
             </div>
+            }
+            
+            {/* Shows Image if animation isn't playing */}
+            {!clickedLeft && !clickedRight ? <Image id="central-image" alt="Cannot find Image" className="event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage]?.eventImg} /> : null } 
+
+            {/* Show Left Arrow Animation */}
+            {clickedLeft ?             
+            <div id="event-slider-holder" className="slide-left event-slider-holder">
+                <Image id="left-image" alt="Cannot find Image" className="event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage]?.eventImg} />
+                <Image id="right-image" alt="Cannot find Image" className="previous-event-image event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage+1]?.eventImg}/>
+            </div>
+            :
+                null
+            }
+
+            {/* Show Right Arrow Animation */}
+            {clickedRight ?             
+            <div id="event-slider-holder" className="slide-right event-slider-holder">
+                <Image id="left-image" alt="Cannot find Image" className="event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage]?.eventImg} />
+                <Image id="right-image" alt="Cannot find Image" className="previous-event-image event-image" cloud_name="dg9s57jo8" publicId={imageData[currentImage+1]?.eventImg}/>
+            </div>
+            :
+                null
+            }
+
+
+
+            {changeImageCheck ? ///Prevents clicking the arrow when animation is playing
+            <div id="right-slider" className="right-slider">
+                <div className="right-arrow arrow"></div>
+            </div> 
+            :
+            <div onClick={clickRightArrowHome} id="right-slider" className="right-slider">
+                <div className="right-arrow arrow"></div>
+            </div> 
+            }
         </div>
     )
 }
 
+///Put two images on the page(maybe three), depending on if you click the left or right arrow, show certain images and slide these images 
+
+///Make a div element with two images, show certain div depending on arrow press, use conditional rendering?
+
+
+// function clickRightArrowHome(){
+//     if(currentId >= imageData.length-1){///changes ID and sets id back to 0 when at the end
+//         setCurrentImage(-1)
+//     }
+//     document.getElementById('slider-image').classList.add('slide-right')
+//     setChangeImageCheck(true)
+//     document.getElementById('previous-event-image').style.display= "block"
+//     document.getElementById('previous-event-image').classList.add('slide-right')
+
+
+//     ///Removes slide left class after the slide is done
+//     setTimeout(() =>{
+//         document.getElementById('slider-image').classList.remove('slide-right')
+//         ///Variable for conditional rendering to remove onClick until animation is finished
+//         setChangeImageCheck(false)
+//         document.getElementById('previous-event-image').style.display= "none"
+//     }, 1000)
+
+
+//     setCurrentImage(prev => prev + 1) //Adds 1 to ID
+//     currentId = currentImage;
+// }
