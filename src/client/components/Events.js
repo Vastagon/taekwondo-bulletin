18fc241c623d6c68566b9c9bd1e5a949b6bf69f9
Vastagon <jacob.yancey29@yahoo.com>
2022-04-08 09:47:44 -0500

    fixing some typos

--- src/client/components/Events.js
+++ src/client/components/Events.js
@@ -16,7 +16,7 @@ export default function Events({auth}){
 
 ///Gets data for events and sets the state of eventCardInfo
 useEffect(() =>{
-    axios.get("https://taekwondo-bulletin.herokuapp.com//eventsinfo")
+    axios.get("https://taekwondo-bulletin.herokuapp.com/eventsinfo")
     .then(res => setEventCardInfo(res))
     .catch(err => console.log(err))
 }, [])
